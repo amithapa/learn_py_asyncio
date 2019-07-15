02527d685e953d13adfb201d8b803da735a3088b
amithapa <amithapa1994@gmail.com>
2019-07-15 14:20:49 +0530

    Create wait.py

---
+++ tutorial_edge/asyncio_tasks/wait.py
@@ -0,0 +1,16 @@
+import asyncio
+
+async def myWorker():
+    print("Hello world")
+
+async def main():
+    print("My Main")
+
+loop = asyncio.get_event_loop()
+
+try:
+    loop.run_until_complete(asyncio.wait([myWorker() for i in range(5)]))
+except KeyboardInterrupt:
+    pass
+finally:
+    loop.close()
