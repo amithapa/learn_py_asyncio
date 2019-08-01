c5314bb4560924550d1d87f39aab86b5feac0e0c
amithapa <amithapa1994@gmail.com>
2019-08-01 12:00:41 +0530

    Create socket_aiohttp.py

---
+++ tutorial_edge/aiohttp/socket/simple_app/socket_aiohttp.py
@@ -0,0 +1,34 @@
+from aiohttp import web
+import socketio
+
+# creates a new Async Socket IO Server
+
+sio = socketio.AsyncServer()
+
+# Creates a new Aiohttp web Application
+app = web.Application()
+# Binds our Socket.IO server to  our Web App instance
+sio.attach(app)
+
+# We can define aiohttp endpoints just as we normally
+# would with no change
+async def index(request):
+    with open("index.html") as html_file:
+        return web.Response(text=html_file.read(), content_type="text/html")
+
+# If we wanted to create a new websocket endpoint
+# use this decorator, passing in the name of the
+# event we wish to listen out for
+@sio.on("message")
+async def print_message(sid, message):
+    # When we receive a new event of type
+    # 'message' through a socket.io connection
+    # we print the socket ID and the message
+    print("Socket ID: ", sid)
+    print(message)
+
+# we bind our aiohttp endpoint to our app router
+app.router.add_get("/", index)
+
+if __name__ == '__main__':
+    web.run_app(app, host="*", port=8081)
